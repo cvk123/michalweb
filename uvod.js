9733b734e31363c8b348bfac0e46849c85c5d6fe
cvk123 <pavelvintr11@seznam.cz>
2023-06-04 01:27:13 +0200

    update3

--- uvod.js
+++ uvod.js
@@ -1,10 +1,10 @@
-
+/**** NAVBAR ANIMACE */
     window.addEventListener('scroll', function() {
       var navbar = document.querySelector('.navbar');
       var modryPruh = document.querySelector('.modry-pruh-navbar');
       var navbarOdkazy = document.querySelector('.navbar-odkazy');
 
-      if (window.scrollY > 0) {
+      if (window.scrollY > 50) {
         navbar.style.top = '0';
         modryPruh.style.top = '0';
         navbarOdkazy.style.bottom = '60px';
@@ -17,6 +17,25 @@
     });
 
 
+    /**** LOGO ANIMACE v navbaru */
+
+    window.addEventListener('scroll', function() {
+      var logo = this.document.querySelector('.logo');
+
+      if (window.scrollY > 150) {
+        logo.style.opacity = '1'; 
+      } else {
+        logo.style.opacity = '0'; 
+      }
+    });
+
+    document.querySelector('.logo').addEventListener('click', function() {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+
+
+/*** Active classy a odkazy */
 var odkazy = document.querySelectorAll('.navbar-odkazy a');
 
 odkazy.forEach(function(odkaz) {
